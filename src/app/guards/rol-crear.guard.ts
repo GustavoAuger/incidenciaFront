cb58f2e1401b594c4b0034d824f90a066449319c
mjcalderon <m.calderon@ext.redcapital.cl>
2025-06-21 00:47:20 -0400

    Se agrega gestor al crear guard para poder abrir el detalle de incidencia

--- src/app/guards/rol-crear.guard.ts
+++ src/app/guards/rol-crear.guard.ts
@@ -11,8 +11,8 @@ export class RolCrearGuard implements CanActivate {
   canActivate(): boolean {
     const roleId = localStorage.getItem('id_rol');
     
-    // Verificar si el rol es 1 (admin), 2 (emisor) o 4 (tienda)
-    if (['1', '2', '4'].includes(roleId || '')) {
+    // Verificar si el rol es 1 (admin), 2 (emisor), 3 (gestor) o 4 (tienda)
+    if (['1', '2', '3', '4'].includes(roleId || '')) {
       return true;
     } else {
       // Redireccionar a home si no tiene permiso
